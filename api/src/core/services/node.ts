6a728c6d05e7beacc3d2862948bcad1554f4bd81
Barend Erasmus <developersworkspace@gmail.com>
2017-02-23 11:56:49 +0200

    adds getStatus and getClusterDetails

--- api/src/core/services/node.ts
+++ api/src/core/services/node.ts
@@ -9,8 +9,8 @@ export class NodeService {
 
     private mongoUrl: string;
 
-    constructor(private redisClient: redis.RedisClient, private mongoClient: mongodb.MongoClient, config: any) {
-        this.mongoUrl = 'mongodb://localhost:27017/myproject';
+    constructor(private redisProvider: any, private mongoClient: mongodb.MongoClient, config: any) {
+        this.mongoUrl = config.mongoUrl;
     }
 
     createNode(clusterName: string, ipAddress: string, port: number) {
@@ -43,7 +43,7 @@ export class NodeService {
     }
 
 
-    findByClusterName(clusterName: string) {
+    list(clusterName: string) {
         return new Promise((resolve: Function, reject: Function) => {
             this.mongoClient.connect(this.mongoUrl, (err: Error, db: mongodb.Db) => {
 
@@ -57,6 +57,79 @@ export class NodeService {
                     db.close();
                 });
             });
+        });
+    }
+
+    getStatus(ipAddress: string, port: number) {
+        return new Promise((resolve: Function, reject: Function) => {
+            let redisClient: redis.RedisClient = this.redisProvider.createClient({
+                host: ipAddress,
+                port: port
+            });
+
+            redisClient.ping((err: Error, result: any) => {
+                if (err) {
+                    resolve(false);
+                } else if (result == "PONG") {
+                    resolve(true);
+                } else {
+                    resolve(false);
+                }
+            });
+        });
+    }
+
+
+    getClusterDetails(clusterName: string) {
+        return this.list(clusterName).then((nodes: Node[]) => {
+
+            let promisesList = nodes.map(x => this.getNodeDetails(x.ipAddress, x.port));
+
+            return Promise.all(promisesList).then((values: any[]) => {
+                return {
+                    used_memory: values.filter(x => x.role == 'master').map(x => x.used_memory).reduce((a, b) => {
+                        return a + b;
+                    }),
+                    expired_keys: values.filter(x => x.role == 'master').map(x => x.expired_keys).reduce((a, b) => {
+                        return a + b;
+                    }),
+                    evicted_keys: values.filter(x => x.role == 'master').map(x => x.evicted_keys).reduce((a, b) => {
+                        return a + b;
+                    }),
+                    connected_clients: values.map(x => x.connected_clients).reduce((a, b) => {
+                        return a + b;
+                    })
+                };
+            });
+        });
+    }
+
+
+    private getNodeDetails(ipAddress: string, port: number) {
+        return new Promise((resolve: Function, reject: Function) => {
+            let redisClient: redis.RedisClient = this.redisProvider.createClient({
+                host: ipAddress,
+                port: port
+            });
+
+            redisClient.info((err: Error, result: any) => {
+
+                let arr = result.split(/\r?\n/).map(x => {
+                    return {
+                        key: x.split(':')[0],
+                        value: x.split(':')[1]
+                    };
+                });
+
+                resolve({
+                    used_memory: arr.filter(z => z.key == 'used_memory')[0].value,
+                    expired_keys: arr.filter(z => z.key == 'expired_keys')[0].value,
+                    evicted_keys: arr.filter(z => z.key == 'evicted_keys')[0].value,
+                    connected_clients: arr.filter(z => z.key == 'connected_clients')[0].value,
+                    role: arr.filter(z => z.key == 'role')[0].value
+                });
+            });
+
         });
     }
 
--- tests/src/core/services/node_test.ts
+++ tests/src/core/services/node_test.ts
@@ -6,6 +6,9 @@ import { expect } from 'chai';
 import * as mongodb from 'mongo-mock';
 import * as redis from 'redis-mock';
 
+//
+import * as redisIntegration from 'redis';
+
 // Imports services
 import { NodeService } from './../../../../api/src/core/services/node';
 
@@ -31,7 +34,7 @@ describe('NodeService', () => {
         };
 
         let mongoClient = mongodb.MongoClient;
-        let redisClient = redis.createClient();
+        let redisClient = redis;
 
         nodeService = new NodeService(redisClient, mongoClient, config);
 
@@ -59,18 +62,45 @@ describe('NodeService', () => {
         });
     });
 
-    describe('findByClusterName', () => {
+    describe('list', () => {
         it('should return list of nodes given existing cluster name', () => {
-            return nodeService.findByClusterName(existingClusterName).then((result: Node[]) => {
+            return nodeService.list(existingClusterName).then((result: Node[]) => {
                 expect(result).to.be.not.null;
                 expect(result.length).to.be.eq(1);
             });
         });
 
         it('should return list of nodes given non-existing cluster name', () => {
-            return nodeService.findByClusterName(nonExistingClusterName).then((result: Node[]) => {
+            return nodeService.list(nonExistingClusterName).then((result: Node[]) => {
                 expect(result).to.be.not.null;
                 expect(result.length).to.be.eq(0);
+            });
+        });
+    });
+
+    describe('getStatus', () => {
+        it('should return true given ip address and port of active node', () => {
+            return nodeService.getStatus(existingIpAddress, existingPort).then((result: Boolean) => {
+                expect(result).to.be.true;
+            });
+        });
+    });
+
+    describe('getClusterDetails', () => {
+
+        beforeEach(function () {
+            let config = {
+                mongoUrl: 'mongodb://localhost:27017/myproject'
+            };
+
+            let mongoClient = mongodb.MongoClient;
+            let redisClient = redisIntegration;
+            nodeService = new NodeService(redisClient, mongoClient, config);
+        });
+
+        it('should return true given ip address and port of active node', () => {
+            return nodeService.getClusterDetails(existingClusterName).then((result: any) => {
+                console.log(result);
             });
         });
     });
