e4783c534c32af19e2a84f8ae6cc0efebe963911
Barend Erasmus <developersworkspace@gmail.com>
2017-02-24 14:14:49 +0200

    update

--- install.sh
+++ install.sh
@@ -1,53 +1,53 @@
 # -- INSTALL DOCKER --
 
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-sudo apt-get update & apt-cache policy docker-engine
-sudo apt-get install -y docker-engine
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+apt-get update & apt-cache policy docker-engine
+apt-get install -y docker-engine
 
 # -- BUILD AND INSTALL REDIS MANAGER --
 
 # Update machine package indexes
-sudo apt-get update
+apt-get update
 
 # Download and run script to install node 7
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_7.x | bash -
 
 # Install node 7
-sudo apt-get install -y nodejs
+apt-get install -y nodejs
 
 # Install 'typescript' node package
-sudo npm install -g typescript
+npm install -g typescript
 
 # Install 'gulp' node package
-sudo npm install -g gulp
+npm install -g gulp
 
 # Install 'angular-cli' node package
-sudo npm install -g @angular/cli
+npm install -g @angular/cli
 
-# Clone 'Authentication' repository
-sudo git clone https://github.com/developersworkspace/Redis-Manager.git
+# Clone 'Redis-Manager' repository
+git clone https://github.com/developersworkspace/Redis-Manager.git
 
 # Change directory to 'api'
-sudo  cd ./Redis-Manager/api
+cd ./Redis-Manager/api
 
 # Install node packages for 'api'
-sudo npm install
+npm install
 
 # Build 'api'
-sudo npm run build
+npm run build
 
 # Change directory to 'web'
-sudo cd ./../web
+cd ./../web
 
 # Install node packages for 'web'
-sudo npm install
+npm install
 
 # Build 'web'
-sudo npm run build
+npm run build
 
 # Change to root of repository
-sudo cd ./../
+cd ./../
 
 # Build and run docker compose as deamon
-sudo docker-compose up -d
+docker-compose up -d
