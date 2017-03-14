dafb7ec6e968d4a4254cf48c409f25c7667b1cf7
developersworkspace@gmail.com <developersworkspace@gmail.com>
2017-03-14 18:42:58 +0200

    Adds cluster detail to keys component

--- web/src/app/keys/keys.component.ts
+++ web/src/app/keys/keys.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+// Imports for HTTP
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { environment } from './../../environments/environment';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
 @Component({
   selector: 'app-keys',
   templateUrl: './keys.component.html',
@@ -9,13 +15,32 @@ import { ActivatedRoute } from '@angular/router';
 export class KeysComponent implements OnInit {
 
   clusterName: string;
+  clusterDetails: any = null;
+  keys: string[];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private http: Http, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.clusterName = params['clusterName'];
+      this.keys = ["asdasd", "asdasd", "asdasd"];
+      this.refreshClusterDetails();
     });
+  }
+
+  refreshClusterDetails() {
+
+    if (this.clusterName == null) {
+      return;
+    }
+
+    this.http.get(environment.apiUrl + '/cluster/details?clusterName=' + this.clusterName)
+      .map((res: Response) => res.json())
+      .subscribe((result: any) => {
+        this.clusterDetails = result;
+      }, (err: any) => {
+        console.log(err);
+      });
   }
 
 }
