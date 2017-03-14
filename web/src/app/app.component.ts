384cc10b96b87dce075f79aa5ab6ba2eb67d6dc2
Barend Erasmus <developersworkspace@gmail.com>
2017-03-14 16:23:21 +0200

    adds router module

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import { environment } from './../environments/environment';
 
-// Import RxJs required methods
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-root',
@@ -13,129 +8,11 @@ import 'rxjs/add/operator/catch';
 })
 export class AppComponent implements OnInit {
 
-  clusters: string[] = [];
-
-  selectedClusterName: string = null;
-  nodes: any[] = [];
-  clusterDetails: any = null;
-
-  newNodeClusterName: string;
-  newNodeIpAddress: string;
-  newNodePort: number;
-
-  clearClusterPattern: string;
-
-  constructor(private http: Http) {
+  constructor() {
 
   }
 
   ngOnInit() {
-    this.refreshClusters();
-  }
-
-  onClick_SelectClusterName(clusterName) {
-    this.selectedClusterName = clusterName;
-    this.refreshNodes();
-    this.refreshClusterDetails();
-  }
-
-  onClick_CreateNewNode() {
-    this.http.post(environment.apiUrl + '/node/create', {
-      clusterName: this.newNodeClusterName,
-      ipAddress: this.newNodeIpAddress,
-      port: this.newNodePort
-    })
-      .map((res: Response) => res.json())
-      .subscribe((result: any) => {
-        this.newNodeClusterName = null;
-        this.newNodeIpAddress = null;
-        this.newNodePort = null;
-        this.refreshClusters();
-        this.onClick_SelectClusterName(this.selectedClusterName);
-      }, (err: any) => {
-        console.log(err);
-      });
-  }
-
-  onClick_ClearCluster() {
-
-    if (this.selectedClusterName == null) {
-      return;
-    }
-
-    this.http.post(environment.apiUrl + '/cluster/clear', {
-      clusterName: this.selectedClusterName,
-      pattern: this.clearClusterPattern
-    })
-      .map((res: Response) => res.json())
-      .subscribe((result: any) => {
-        this.clearClusterPattern = null;
-        this.refreshClusterDetails();
-      }, (err: any) => {
-        console.log(err);
-      });
-  }
-
-  refreshNodes() {
-
-    if (this.nodes == null) {
-      return;
-    }
-
-    this.nodes = null;
-
-    this.http.get(environment.apiUrl + '/node/list?clusterName=' + this.selectedClusterName)
-      .map((res: Response) => res.json())
-      .subscribe((result: any) => {
-        this.nodes = result;
-        this.updateNodeStatuses();
-      }, (err: any) => {
-        console.log(err);
-      });
-  }
-
-  refreshClusterDetails() {
-
-    if (this.selectedClusterName == null) {
-      return;
-    }
-
-
-    this.http.get(environment.apiUrl + '/cluster/details?clusterName=' + this.selectedClusterName)
-      .map((res: Response) => res.json())
-      .subscribe((result: any) => {
-        this.clusterDetails = result;
-      }, (err: any) => {
-        console.log(err);
-      });
-  }
-
-  refreshClusters() {
-    this.http.get(environment.apiUrl + '/cluster/list')
-      .map((res: Response) => res.json())
-      .subscribe((result: any) => {
-        this.clusters = result;
-      }, (err: any) => {
-        console.log(err);
-      });
-  }
-
-  updateNodeStatuses() {
-
-    if (this.nodes == null) {
-      return;
-    }
-
-    for (let i = 0; i < this.nodes.length; i++) {
-      let node = this.nodes[i];
-
-      this.http.get(environment.apiUrl + '/node/status?ipAddress=' + node.ipAddress + '&port=' + node.port)
-        .map((res: Response) => res.json())
-        .subscribe((result: any) => {
-          node.isActive = result;
-        }, (err: any) => {
-          console.log(err);
-        });
-    }
+    
   }
 }
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,18 +1,32 @@
+// Imports modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
+// Imports Components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+
+let router = RouterModule.forRoot([
+  {
+    path: '',
+    component: HomeComponent
+  }
+]);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    router,
   ],
   providers: [],
   bootstrap: [AppComponent]
